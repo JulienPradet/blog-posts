952aea6d395fa4aacb655dbe51f4f3f461eb2057
Julien <julien.pradet@gmail.com>
2017-02-12 12:38:15 +0100

    Defer spinner display in order to improve felt performance

--- src/site/Loading.js
+++ src/site/Loading.js
@@ -1,5 +1,28 @@
 import React from 'react'
 
-const Loading = () => <div className='spinner' ariaLabel='Chargement en cours...' />
+class Loading extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      displaySpinner: false
+    }
+  }
+
+  componentDidMount () {
+    this.deferLoading = setTimeout(() => {
+      this.setState({displaySpinner: true})
+    }, 100)
+  }
+
+  componentWillUnmount () {
+    clearTimeout(this.deferLoading)
+  }
+
+  render () {
+    return (
+      <div className={'spinner ' + (this.state.displaySpinner ? '' : 'spinner--hidden')} ariaLabel='Chargement en cours...' />
+    )
+  }
+}
 
 export default Loading
