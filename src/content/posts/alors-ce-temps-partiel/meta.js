080567e8e99d19c48d398ca27289704c621fdea9
Julien <julien.pradet@gmail.com>
2017-03-31 12:01:11 +0200

    Ajout de ma retrospective sur le temps partiel

---
+++ src/content/posts/alors-ce-temps-partiel/meta.js
@@ -0,0 +1,7 @@
+module.exports = {
+  isListed: true,
+  title: 'Alors, ce temps partiel ?',
+  date: '2017-03-31',
+  description: 'Cela fait maintenant 3 mois que je suis passé à temps partiel et que j\'ai ouvert ce blog. Conclusion ?',
+  isPost: true
+}
