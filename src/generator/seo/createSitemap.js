d4042ca7e130b62383638ca7fc765415e249eaba
Julien <julien.pradet@gmail.com>
2017-04-28 15:47:20 +0200

    Fix sitemap url

--- src/generator/seo/createSitemap.js
+++ src/generator/seo/createSitemap.js
@@ -1,35 +1,32 @@
-const path = require('path')
-const fs = require('../util/fs')
-const log = require('../util/log')('SEO')
-const getMetas = require('./getMetas')
-const sitemap = require('sitemap')
+const path = require('path');
+const fs = require('../util/fs');
+const log = require('../util/log')('SEO');
+const getMetas = require('./getMetas');
+const sitemap = require('sitemap');
 
-const createRss = (paths) => () => {
-  log('info', 'Creating sitemap')
+const createRss = paths =>
+  () => {
+    log('info', 'Creating sitemap');
 
-  const url = 'https://www.julienpradet.fr/'
+    const url = 'https://www.julienpradet.fr/';
 
-  return getMetas(paths)()
-    .map((metas) => {
-      return sitemap.createSitemap({
-        hostname: url,
-        urls: [
-          { url: '/', changefreq: 'weekly' },
-          ...metas.map((meta) => ({
-            url: meta.location + '/',
-            lastmodISO: (new Date(meta.date)).toISOString()
-          }))
-        ]
+    return getMetas(paths)()
+      .map(metas => {
+        return sitemap.createSitemap({
+          hostname: url,
+          urls: [
+            { url: '/', changefreq: 'weekly' },
+            ...metas.map(meta => ({
+              url: meta.location,
+              lastmodISO: new Date(meta.date).toISOString()
+            }))
+          ]
+        });
       })
-    })
-    .map((sitemap) => sitemap.toString())
-    .flatMap((sitemap) => (
-      fs.writefile(
-        path.join(paths.buildPath, 'sitemap.xml'),
-        sitemap
-      )
-    ))
-    .do((path) => log('success', `Sitemap created`))
-}
+      .map(sitemap => sitemap.toString())
+      .flatMap(sitemap =>
+        fs.writefile(path.join(paths.buildPath, 'sitemap.xml'), sitemap))
+      .do(path => log('success', `Sitemap created`));
+  };
 
-module.exports = createRss
+module.exports = createRss;
