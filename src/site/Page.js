4189927da9d3fcbbd5a2001d55c39a79900f2f19
Julien <julien.pradet@gmail.com>
2017-06-05 13:33:07 +0200

    Add ability to add external pages

--- src/site/Page.js
+++ src/site/Page.js
@@ -1,104 +1,151 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import Helmet from 'react-helmet'
-import {withSite} from './Site'
-import PageHeader from './PageHeader'
-import PageContent from './PageContent'
-import PageFooter from './PageFooter'
-import PageList from './PageList'
-import GlobalFooter from './GlobalFooter'
+import React from "react";
+import { Link } from "react-router-dom";
+import Helmet from "react-helmet";
+import { withSite } from "./Site";
+import PageHeader from "./PageHeader";
+import PageContent from "./PageContent";
+import PageFooter from "./PageFooter";
+import PageList from "./PageList";
+import GlobalFooter from "./GlobalFooter";
 
-const DefaultPage = (props) => (
+const DefaultPage = props => (
   <div>
     <PageHeader page={props.page} />
     <PageContent>
       {props.children}
     </PageContent>
   </div>
-)
+);
 
-const Post = (props) => (
-  <div itemScope itemType='http://schema.org/Article'>
+const Post = props => (
+  <div itemScope itemType="http://schema.org/Article">
     <PageHeader page={props.page} isPost />
     <PageContent isPost>
       {props.children}
     </PageContent>
     <hr />
-    <div className='page-content'>
+    <div className="page-content">
       <PageFooter isPost />
     </div>
   </div>
-)
+);
+
+const makeMeta = props => [
+  { charset: "utf-8" },
+  { "http-equiv": "X-UA-Compatible", content: "IE=edge" },
+  {
+    name: "viewport",
+    content: "width=device-width, initial-scale=1"
+  },
+  { name: "description", content: props.page.description },
+  { name: "theme-color", content: "#00C9C9" },
+  { property: "twitter:site", content: "@JulienPradet" },
+  { property: "twitter:card", content: "summary" },
+  { property: "twitter:creator", content: "@JulienPradet" },
+  { property: "twitter:title", content: props.page.title },
+  {
+    property: "twitter:description",
+    content: (props.page.twitter && props.page.twitter.description) ||
+      props.page.description
+  },
+  {
+    property: "twitter:image",
+    content: props.page.image ||
+      "https://pbs.twimg.com/profile_images/424964348461600768/aygHDGpF.png"
+  },
+  {
+    property: "og:site_name",
+    content: props.site.meta.site_name
+  },
+  { property: "og:type", content: "page" },
+  { property: "og:title", content: props.page.title },
+  {
+    property: "og:url",
+    content: `${props.site.meta.homepage}${props.path}`
+  },
+  {
+    property: "og:image",
+    content: props.page.image ||
+      "https://pbs.twimg.com/profile_images/424964348461600768/aygHDGpF.png"
+  },
+  { property: "og:description", content: props.page.description }
+];
+const makeLink = props => [
+  {
+    rel: "canonical",
+    href: `${props.site.meta.homepage}${props.path}`
+  },
+  { rel: "preload", href: "/css/main.css", as: "style" },
+  { rel: "stylesheet", href: `/css/main.css` },
+  {
+    rel: "shortcut icon",
+    href: "/favicon.ico",
+    type: "image/x-icon"
+  },
+  { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+  { rel: "manifest", href: "/manifest.json" },
+  {
+    rel: "alternate",
+    type: "application/rss+xml",
+    title: "Le flux RSS de Julien Pradet",
+    href: `${props.site.meta.homepage}/feed.xml`
+  }
+];
 
 class Page extends React.Component {
-  constructor () {
-    super()
-    this.state = {appendCodeCss: false}
+  constructor() {
+    super();
+    this.state = { appendCodeCss: false };
   }
 
-  componentDidMount () {
-    this.setState({appendCodeCss: true})
+  componentDidMount() {
+    this.setState({ appendCodeCss: true });
   }
 
-  render () {
-    return (
-      <div className='page'>
-        <Helmet
-          titleTemplate={`%s | ${this.props.site.meta.author.name}`}
-          defaultTitle='Blog'
-          title={this.props.page.title}
-          meta={[
-            {charset: 'utf-8'},
-            {'http-equiv': 'X-UA-Compatible', content: 'IE=edge'},
-            {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-            {name: 'description', content: this.props.page.description},
-            {name: 'theme-color', content: '#00C9C9'},
-            {property: 'twitter:site', content: '@JulienPradet'},
-            {property: 'twitter:card', content: 'summary'},
-            {property: 'twitter:creator', content: '@JulienPradet'},
-            {property: 'twitter:title', content: this.props.page.title},
-            {property: 'twitter:description', content: (this.props.page.twitter && this.props.page.twitter.description) || this.props.page.description},
-            {property: 'twitter:image', content: this.props.page.image || 'https://pbs.twimg.com/profile_images/424964348461600768/aygHDGpF.png'},
-            {property: 'og:site_name', content: this.props.site.meta.site_name},
-            {property: 'og:type', content: 'page'},
-            {property: 'og:title', content: this.props.page.title},
-            {property: 'og:url', content: `${this.props.site.meta.homepage}${this.props.path}`},
-            {property: 'og:image', content: this.props.page.image || 'https://pbs.twimg.com/profile_images/424964348461600768/aygHDGpF.png'},
-            {property: 'og:description', content: this.props.page.description}
-          ]}
-          link={[
-            {rel: 'canonical', href: `${this.props.site.meta.homepage}${this.props.path}`},
-            {rel: 'preload', href: '/css/main.css', as: 'style'},
-            {rel: 'stylesheet', href: `/css/main.css`},
-            {rel: 'shortcut icon', href: '/favicon.ico', type: 'image/x-icon'},
-            {rel: 'icon', href: '/favicon.ico', type: 'image/x-icon'},
-            {rel: 'manifest', href: '/manifest.json'},
-            {rel: 'alternate', type: 'application/rss+xml', title: 'Le flux RSS de Julien Pradet', href: `${this.props.site.meta.homepage}/feed.xml`}
-          ]}
-        />
-        <div>
-          {this.props.page.isPost
-            ? (
-              <Post page={this.props.page}>
-                {this.props.children}
-              </Post>
-            )
-            : (
-              <DefaultPage page={this.props.page}>
-                {this.props.children}
-              </DefaultPage>
-            )}
-          <hr />
-          <div className='page-content'>
-            {!this.props.page.standalone && (
-              <PageList length={this.props.page.isHome ? 0 : 2} />
-            )}
-            {!this.props.page.isHome && <Link to='/'>Revenir à la page d'accueil</Link>}
+  render() {
+    console.log(this.props.page.title);
+    const helmet = (
+      <Helmet
+        titleTemplate={`%s | ${this.props.site.meta.author.name}`}
+        defaultTitle="Blog"
+        title={this.props.page.title}
+        meta={makeMeta(this.props)}
+        link={makeLink(this.props)}
+      />
+    );
+
+    if (
+      typeof this.props.page.isPage === "undefined" || this.props.page.isPage
+    ) {
+      return (
+        <div className="page">
+          {helmet}
+          <div>
+            {this.props.page.isPost
+              ? <Post page={this.props.page}>
+                  {this.props.children}
+                </Post>
+              : <DefaultPage page={this.props.page}>
+                  {this.props.children}
+                </DefaultPage>}
+            <hr />
+            <div className="page-content">
+              {!this.props.page.standalone &&
+                <PageList length={this.props.page.isHome ? 0 : 2} />}
+              {!this.props.page.isHome &&
+                <Link to="/">Revenir à la page d'accueil</Link>}
+            </div>
           </div>
+          <GlobalFooter />
         </div>
-        <GlobalFooter />
-      </div>
-    )
+      );
+    } else {
+      return (
+        <div className="page">
+          {this.props.children}
+        </div>
+      );
+    }
   }
 }
 
@@ -128,15 +175,15 @@ Page.propTypes = {
   isHome: React.PropTypes.bool,
   isPost: React.PropTypes.bool,
   children: React.PropTypes.node.isRequired
-}
+};
 
 Page.defaultProps = {
   isHome: false,
   standalone: false
-}
+};
 
 Page.defaultProps = {
   page: {}
-}
+};
 
-export default withSite(Page)
+export default withSite(Page);
--- src/site/Site.js
+++ src/site/Site.js
@@ -1,49 +1,48 @@
-import React from 'react'
-import getDisplayName from './util/getDisplayName'
+import React from "react";
+import getDisplayName from "./util/getDisplayName";
 
 class SiteProvider extends React.Component {
-  getChildContext () {
+  getChildContext() {
     return {
       site: {
         meta: this.props.meta,
-        pages: this.props.pages.map((page) => {
-          return Object.assign(
-            {},
-            page,
-            { date: page.date && new Date(page.date) }
-          )
+        pages: this.props.pages.map(page => {
+          return Object.assign({}, page, {
+            date: page.date && new Date(page.date),
+            isPage: typeof page.isPage === "undefined" ? true : page.isPage
+          });
         })
       }
-    }
+    };
   }
 
-  render () {
-    return React.Children.only(this.props.children)
+  render() {
+    return React.Children.only(this.props.children);
   }
 }
 
 SiteProvider.childContextTypes = {
   site: React.PropTypes.object.isRequired
-}
+};
 
 SiteProvider.propTypes = {
   meta: React.PropTypes.any.isRequired,
   pages: React.PropTypes.any.isRequired,
   children: React.PropTypes.node.isRequired
-}
+};
 
-export const withSite = (Component) => {
+export const withSite = Component => {
   const withSiteComponent = (props, context) => (
     <Component site={context.site} {...props} />
-  )
+  );
 
   withSiteComponent.contextTypes = {
     site: React.PropTypes.object.isRequired
-  }
+  };
 
-  withSiteComponent.displayName = `withSite(${getDisplayName(Component)})`
+  withSiteComponent.displayName = `withSite(${getDisplayName(Component)})`;
 
-  return withSiteComponent
-}
+  return withSiteComponent;
+};
 
-export default SiteProvider
+export default SiteProvider;
