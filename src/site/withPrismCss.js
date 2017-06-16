f815ecdbfb95195c9531bc7b3cafc97b33ea1367
Julien <julien.pradet@gmail.com>
2017-06-16 14:15:21 +0200

    Defer loading of prism css

--- src/site/withPrismCss.js
+++ src/site/withPrismCss.js
@@ -1,13 +1,32 @@
-import React from 'react'
-import Helmet from 'react-helmet'
+import React from "react";
+import Helmet from "react-helmet";
 
-export default () => (BaseComponent) => {
-  return class extends React.Component {
-    render () {
-      return <div>
-        <Helmet link={[{rel: 'stylesheet', href: '/css/prism-onedark.css'}]} />
-        <BaseComponent {...this.props} />
-      </div>
-    }
-  }
-}
+export default () =>
+  BaseComponent => {
+    return class extends React.Component {
+      constructor() {
+        super();
+        this.state = {
+          loadCss: false
+        };
+      }
+
+      componentDidMount() {
+        this.setState({
+          loadCss: true
+        });
+      }
+
+      render() {
+        return (
+          <div>
+            {this.state.loadCss &&
+              <Helmet
+                link={[{ rel: "stylesheet", href: "/css/prism-onedark.css" }]}
+              />}
+            <BaseComponent {...this.props} />
+          </div>
+        );
+      }
+    };
+  };
