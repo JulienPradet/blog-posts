46b44a06892bc45640347291a1be9f87cf0b665e
Julien <julien.pradet@gmail.com>
2017-06-16 14:16:07 +0200

    Ensure there is a trailing slash (SEO)

--- src/site/PagePreview.js
+++ src/site/PagePreview.js
@@ -1,29 +1,34 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import {formatDate} from './dateFormats'
+import React from "react";
+import { Link } from "react-router-dom";
+import { formatDate } from "./dateFormats";
 
-const PagePreview = ({page}) => (
-  <div className='page-preview'>
-    <Link to={'/' + page.location} className='page-preview__title'>
+const ensureSlashs = url => {
+  if (!url.startsWith("/")) url = `/${url}`;
+  if (!url.endsWith("/")) url = `${url}/`;
+  return url;
+};
+
+const PagePreview = ({ page }) => (
+  <div className="page-preview">
+    <Link to={ensureSlashs(page.location)} className="page-preview__title">
       {page.title}
     </Link>
-    <div className='page-preview__meta'>
-      {page.date && (
+    <div className="page-preview__meta">
+      {page.date &&
         <time>
           {formatDate(page.date)}
-        </time>
-      )}
+        </time>}
     </div>
-    <div className='page-preview__description'>
+    <div className="page-preview__description">
       {page.description}
-      {' '}
+      {" "}
     </div>
-    <div className='page-preview__read-more'>
-      <Link to={'/' + page.location}>
+    <div className="page-preview__read-more">
+      <Link to={ensureSlashs(page.location)}>
         Lire la suite →
       </Link>
     </div>
   </div>
-)
+);
 
-export default PagePreview
+export default PagePreview;
