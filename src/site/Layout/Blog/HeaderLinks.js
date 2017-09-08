bf7c3082aa5477f0592e94dae0e82003e6069f23
Julien <julien.pradet@gmail.com>
2017-09-08 16:32:33 +0200

    Finalize theme and colors

--- src/site/Layout/Blog/HeaderLinks.js
+++ src/site/Layout/Blog/HeaderLinks.js
@@ -2,47 +2,62 @@ import React, { Component } from "react";
 import PageLink from "../../components/PageLink";
 
 const CategoryLink = ({ category, path }) =>
-  <PageLink to={path}>{category}</PageLink>;
+  <PageLink to={path}>
+    {category}
+  </PageLink>;
 
-const Menu = ({ isOpened }) =>
-  <div className={`menu menu--${isOpened ? "opened" : "closed"}`}>
-    <ul>
-      <li>
-        <CategoryLink category="Tutoriels" path="/tutoriels/" />
-      </li>
-      <li>
-        <CategoryLink category="Fiches Techniques" path="/fiches-techniques/" />
-      </li>
-      <li>
-        <CategoryLink category="Explorations" path="/explorations/" />
-      </li>
-      <li>
-        <CategoryLink category="Autres" path="/autres/" />
-      </li>
-      <li>
-        <PageLink to="/a-propos/">A propos</PageLink>
-      </li>
-    </ul>
-    <ul>
-      <li>
-        <a href="mailto:julien@pradet.me?body=Hi! I found your mail on your blog and...">
-          julien@pradet.me
-        </a>
-      </li>
-      <li>
-        <a href="https://twitter.com/JulienPradet">
-          @JulienPradet
-        </a>
-      </li>
-      <li>
-        <a href="https://github.com/JulienPradet">
-          JulienPradet
-        </a>
-      </li>
-      <li>
-        <PageLink to="/cv/">CV</PageLink>
-      </li>
-    </ul>
+const Menu = ({ isOpened, toggleMenu }) =>
+  <div
+    className={`menu__wrapper menu__wrapper--${isOpened ? "opened" : "closed"}`}
+  >
+    {isOpened && <div className="menu__overlay" onClick={toggleMenu} />}
+    <div className={`menu menu--${isOpened ? "opened" : "closed"}`}>
+      <div className="menu__content">
+        <div className="menu__content__item">
+          <h3>Catégories</h3>
+          <ul>
+            <li>
+              <CategoryLink category="Tutoriels" path="/tutoriels/" />
+            </li>
+            <li>
+              <CategoryLink
+                category="Fiches Techniques"
+                path="/fiches-techniques/"
+              />
+            </li>
+            <li>
+              <CategoryLink category="Explorations" path="/explorations/" />
+            </li>
+            <li>
+              <CategoryLink category="Autres" path="/autres/" />
+            </li>
+          </ul>
+        </div>
+        <div className="menu__content__item">
+          <h3>Me contacter</h3>
+          <ul>
+            <li>
+              <a href="mailto:julien@pradet.me?body=Hi! I found your mail on your blog and...">
+                julien@pradet.me
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/JulienPradet">
+                @JulienPradet
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/JulienPradet">
+                JulienPradet
+              </a>
+            </li>
+            <li>
+              <PageLink to="/cv/">CV</PageLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>;
 
 class HeaderLinks extends Component {
@@ -78,7 +93,7 @@ class HeaderLinks extends Component {
             <span>+</span>
           </button>
         </div>
-        <Menu isOpened={this.state.opened} />
+        <Menu isOpened={this.state.opened} toggleMenu={this.toggleMenu} />
       </div>
     );
   }
