c093d040076ff3a45583ecb3f1a478bf7e31281d
Julien <julien.pradet@gmail.com>
2017-10-06 14:26:01 +0200

    Scrolltop whenever the blog content changes

--- src/site/Layout/Blog/Blog.js
+++ src/site/Layout/Blog/Blog.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import Helmet from "react-helmet";
 import blogCss from "./css/blog_critical.scss";
 import Header from "./Header";
@@ -11,76 +11,85 @@ import { withSite } from "../../Site";
 import { PageTitleProvider } from "../../components/PageTitle";
 import PageLink from "../../components/PageLink";
 
-const BlogContent = props => {
-  return (
-    <div
-      itemScope
-      itemType="http://schema.org/Article"
-      className={`blog__content ${props.page.isHome
-        ? "blog__content--home"
-        : ""}`}
-    >
-      <span
-        dangerouslySetInnerHTML={{
-          __html: `
+class BlogContent extends Component {
+  componentDidUpdate(prevProps) {
+    if (prevProps.path !== this.props.path) {
+      document.body.scrollTop = 0;
+    }
+  }
+
+  render() {
+    const props = this.props;
+    return (
+      <div
+        itemScope
+        itemType="http://schema.org/Article"
+        className={`blog__content ${props.page.isHome
+          ? "blog__content--home"
+          : ""}`}
+      >
+        <span
+          dangerouslySetInnerHTML={{
+            __html: `
             <span itemscope itemtype="http://schema.org/Person" itemprop="author">
               <meta itemprop="name" content="Julien Pradet" />
             </span>
           `
-        }}
-      />
-      <div>
-        {(!props.page.isHome || props.page.filter) && (
-            <div className="blog__content__title">
-              <h1 {...(!props.page.isHome ? { itemProp: "headline" } : {})}>
-                <PageLink
-                  to={props.path}
-                  {...(!props.page.isHome
-                    ? { itemProp: "mainEntityOfPage" }
-                    : {})}
-                >
-                  {props.page.title}
-                </PageLink>
-              </h1>
+          }}
+        />
+        <div>
+          {(!props.page.isHome || props.page.filter) && (
+              <div className="blog__content__title">
+                <h1 {...(!props.page.isHome ? { itemProp: "headline" } : {})}>
+                  <PageLink
+                    to={props.path}
+                    {...(!props.page.isHome
+                      ? { itemProp: "mainEntityOfPage" }
+                      : {})}
+                  >
+                    {props.page.title}
+                  </PageLink>
+                </h1>
+              </div>
+            )}
+
+          {props.page.date && (
+            <div className="blog__content__time">
+              <time
+                {...(!props.page.isHome ? { itemProp: "datePublished" } : {})}
+                dateTime={new Date(props.page.date).toISOString()}
+              >
+                {formatDate(new Date(props.page.date))}.
+              </time>
             </div>
           )}
 
-        {props.page.date && (
-          <div className="blog__content__time">
-            <time
-              {...(!props.page.isHome ? { itemProp: "datePublished" } : {})}
-              dateTime={new Date(props.page.date).toISOString()}
-            >
-              {formatDate(new Date(props.page.date))}
-            </time>
-          </div>
-        )}
+          <main className="page-content" role="main">
+            {!props.page.isHome ? (
+              <article itemProp="articleBody">{props.children}</article>
+            ) : (
+              props.children
+            )}
+          </main>
 
-        <main className="page-content" role="main">
-          {!props.page.isHome ? (
-            <article itemProp="articleBody">{props.children}</article>
-          ) : (
-            props.children
+          {!props.page.isHome && <hr />}
+          {!props.page.isHome && (
+            <footer className="page-footer">
+              Si vous voulez suivre mes publications, il paraît que j'ai un feed{" "}
+              <a href="/feed.xml">RSS</a> et un{" "}
+              <a href="https://twitter.com/JulienPradet">Twitter</a>
+              .
+              <br />
+              Si vous pensez à d'autres méthodes que vous voudriez que je mette
+              en place (pigeon voyageur, avion en papier, etc.), n'hésitez pas à
+              me les proposer :)
+            </footer>
           )}
-        </main>
-
-        {!props.page.isHome && <hr />}
-        {!props.page.isHome && (
-          <footer className="page-footer">
-            Si vous voulez suivre mes publications, il paraît que j'ai un feed{" "}
-            <a href="/feed.xml">RSS</a> et un{" "}
-            <a href="https://twitter.com/JulienPradet">Twitter</a>
-            .
-            <br />
-            Si vous pensez à d'autres méthodes que vous voudriez que je mette en
-            place (pigeon voyageur, avion en papier, etc.), n'hésitez pas à me
-            les proposer :)
-          </footer>
-        )}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 const Blog = props => {
   const helmet = makeHelmet(props);
