b90da7e0c33840e91cbc80a877b120db01f3b0e5
Julien <julien.pradet@gmail.com>
2017-11-27 23:22:02 +0100

    Add drafts for next two articles about PWAs

---
+++ src/content/fiches-techniques/pwa-declarer-un-service-worker-et-gerer-son-cycle-de-vie/meta.js
@@ -0,0 +1,8 @@
+module.exports = {
+  isListed: false,
+  layout: "Blog",
+  title: "PWA : Déclarer un Service Worker et gérer son cycle de vie",
+  date: "2017-12-08",
+  description: `Après avoir expliqué ce qu'était une PWA et un Service Worker, attaquons nous à l'implémentation de ce dernier. J'y présenterai les bonnes pratiques que j'aurai bien aimé connaître avant de m'y mettre.`,
+  category: "Fiches Techniques"
+};
--- src/content/fiches-techniques/pwa-rendre-un-site-web-disponible-grace-aux-services-workers/meta.js
+++ src/content/fiches-techniques/pwa-rendre-un-site-web-disponible-grace-aux-services-workers/meta.js
@@ -2,7 +2,7 @@ module.exports = {
   isListed: true,
   layout: "Blog",
   title: "PWA : Rendre un site web disponible grâce aux Services Workers",
-  date: "2017-11-24",
+  date: "2017-12-01",
   description: `Les Progressives Web Apps (PWA) est une des actualités du web qui a le plus de hype en ce moment. En quoi ça consiste, et comment le mettre en place ?`,
   category: "Fiches Techniques"
 };
