7fe08d62cfbabe5ff1e43ebb3beb244629c00205
Pof Magicfingers <pof@pof.pm>
2017-12-21 22:10:18 +0100

    Update meta.js

--- src/content/meta.js
+++ src/content/meta.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: "Enchanté, Julien Pradet",
+  title: "Enchanté, commit42",
   description: `Je suis un agglomérat de plein de choses. Je fais de l'illustration, de la 3D (surtout de la sculpture) et un peu de graphisme. Mais je suis surtout un furieux optimiste qui a envie de partager avec les autres.`,
   layout: "Blog",
   isHome: true,
