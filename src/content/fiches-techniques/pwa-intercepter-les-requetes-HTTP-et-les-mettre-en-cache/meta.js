98ff13db3c4158898910392d358c04f2b1034aa9
Julien <julien.pradet@gmail.com>
2017-12-24 11:44:29 +0100

    Fix article date

--- src/content/fiches-techniques/pwa-intercepter-les-requetes-HTTP-et-les-mettre-en-cache/meta.js
+++ src/content/fiches-techniques/pwa-intercepter-les-requetes-HTTP-et-les-mettre-en-cache/meta.js
@@ -2,7 +2,7 @@ module.exports = {
   isListed: true,
   layout: "Blog",
   title: "PWA : Intercepter les requêtes HTTP et les mettre en cache",
-  date: "2017-12-22",
+  date: "2017-12-24",
   description: `Après avoir installé notre Service Worker, il est temps de lui faire faire des choses. Nous verrons comment utiliser l'interception de requêtes et la Cache API afin d'améliorer l'expérience de nos utilisateurs⋅rices.`,
   category: "Fiches Techniques"
 };
