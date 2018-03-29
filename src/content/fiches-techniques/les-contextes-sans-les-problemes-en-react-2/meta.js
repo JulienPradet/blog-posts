86227785e028c61ca15bffbb245524b2e9896bfd
Julien <julien.pradet@gmail.com>
2018-03-29 09:28:57 +0200

    Fix redirect when some letters are capitalized in the url

--- src/content/fiches-techniques/les-contextes-sans-les-problemes-en-react-2/meta.js
+++ src/content/fiches-techniques/les-contextes-sans-les-problemes-en-react-2/meta.js
@@ -5,6 +5,9 @@ module.exports = {
   date: "2017-01-20",
   description: `Nous avons vu dans le post de la semaine dernière que les contextes pouvaient poser des problèmes de mise à jour des composants. Comment peut-on les contourner ?`,
   category: "Fiches Techniques",
-  redirect: ["/posts/Les-contextes-sans-les-problemes-en-react-2"],
-  next: ["/fiches-techniques/les-contextes-sans-les-problemes-en-react"]
+  redirect: [
+    "/posts/Les-contextes-sans-les-problemes-en-react-2",
+    "/fiches-techniques/Les-contextes-sans-les-problemes-en-react-2"
+  ],
+  prev: ["/fiches-techniques/les-contextes-sans-les-problemes-en-react"]
 };
