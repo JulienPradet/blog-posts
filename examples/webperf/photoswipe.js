839eb6fac8f7d3e89a68d219b83ba734ba8902cb
Julien Pradet <julien.pradet@gmail.com>
2023-06-19 15:32:22 +0200

    Defer photoswipe css

--- examples/webperf/photoswipe.js
+++ examples/webperf/photoswipe.js
@@ -1,14 +1,13 @@
 import Lightbox from 'photoswipe/lightbox';
 import PhotoSwipe from 'photoswipe';
 
-import 'photoswipe/dist/photoswipe.css';
-
-
 export function initPhotoswipe() {
-    const lightbox = new Lightbox({
-      gallery: '.article-grid',
-      children: '.photoswipe-image',
-      pswpModule: PhotoSwipe
-    });
-    lightbox.init();
+    import('photoswipe/dist/photoswipe.css');
+
+	const lightbox = new Lightbox({
+		gallery: '.article-grid',
+		children: '.photoswipe-image',
+		pswpModule: PhotoSwipe
+	});
+	lightbox.init();
 }
