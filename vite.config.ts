7f577eac16073af82ad09987a103ebf490de3ee5
Julien Pradet <julien.pradet@gmail.com>
2023-07-03 10:10:58 +0200

    Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitest/config';
 import { plugin as mdPlugin, Mode } from 'vite-plugin-markdown';
 import markdownIt from 'markdown-it';
 import markdownItAnchor from 'markdown-it-anchor';
+import markdownItCheckbox from 'markdown-it-checkbox';
 
 export default defineConfig({
 	plugins: [
@@ -21,17 +22,22 @@ export default defineConfig({
 					html: true,
 					linkify: true,
 					typographer: true
-				}).use(markdownItAnchor, {
-					permalink: true,
-					slugify: (text: string) =>
-						text
-							.replaceAll(/'"’/g, '')
-							.normalize('NFD')
-							.replace(/[\u0300-\u036f]/g, '')
-							.toLowerCase()
-							.replaceAll(/[^a-z0-9-_]+/g, '-')
-							.replace(/(^-+)|(-+$)/, '')
-				});
+				})
+					.use(markdownItAnchor, {
+						permalink: markdownItAnchor.permalink.ariaHidden({
+							space: false,
+							placement: 'after'
+						}),
+						slugify: (text: string) =>
+							text
+								.replaceAll(/'"’/g, '')
+								.normalize('NFD')
+								.replace(/[\u0300-\u036f]/g, '')
+								.toLowerCase()
+								.replaceAll(/[^a-z0-9-_]+/g, '-')
+								.replace(/(^-+)|(-+$)/, '')
+					})
+					.use(markdownItCheckbox);
 
 				md.core.ruler.before('inline', 'french-typo', function replace(state) {
 					const tokens = state.tokens;
