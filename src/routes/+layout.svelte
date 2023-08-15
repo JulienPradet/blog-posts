d4721d19de31b978b8eb0e16cc8a847dbf0a630b
Julien Pradet <julien.pradet@gmail.com>
2023-08-15 20:35:19 +0200

    Setup matomo analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,38 @@
 <script lang="ts">
+	import { page } from '$app/stores';
+
 	if (typeof navigator !== 'undefined' && 'serviceWorker' in window.navigator) {
 		window.navigator.serviceWorker.getRegistrations().then((registrations) => {
 			registrations.forEach((registration) => {
 				registration.unregister();
 			});
 		});
+	}
+
+	var _paq = [];
+	if (typeof window !== 'undefined') {
+		/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+		_paq.push(['trackPageView']);
+		_paq.push(['enableLinkTracking']);
+		(function () {
+			var u = 'https://analytics.julienpradet.fr/';
+			_paq.push(['setTrackerUrl', u + 'matomo.php']);
+			_paq.push(['setSiteId', '1']);
+			var d = document,
+				g = d.createElement('script'),
+				s = d.getElementsByTagName('script')[0];
+			g.async = true;
+			g.src = u + 'matomo.js';
+			s.parentNode.insertBefore(g, s);
+		})();
+	}
+
+	$: {
+		if ($page && typeof window !== 'undefined') {
+			_paq.push(['setCustomUrl', window.location.pathname]);
+			_paq.push(['setDocumentTitle', document.title.replace(' | Julien Pradet', '')]);
+			_paq.push(['trackPageView']);
+		}
 	}
 </script>
 
