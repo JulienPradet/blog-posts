f037db6cc554bb724c8ae30dffbe235dbcfdf771
Julien Pradet <julien.pradet@gmail.com>
2023-08-21 10:53:39 +0200

    Fix publication date

--- src/routes/(blog)/tutoriels/comment-detecter-du-cls/meta.ts
+++ src/routes/(blog)/tutoriels/comment-detecter-du-cls/meta.ts
@@ -5,7 +5,7 @@ const meta: ArticleMeta = {
 	type: 'article',
 	isListed: true,
 	title: 'Comment diagnostiquer et corriger du Cumulative Layout Shift (CLS) ?',
-	date: new Date('2023-08-14'),
+	date: new Date('2023-08-21'),
 	description: `On a tous en tête ces sites qui clignotent dans tous les sens pendant leur chargement. Cet effet se mesure via le Cumulative Layout Shift. Qu'est-ce que c'est exactement ? Comment le diagnostiquer ? Quelles bonnes pratiques avoir en tête ?`,
 	category: ArticleCategory['tutoriels']
 };
