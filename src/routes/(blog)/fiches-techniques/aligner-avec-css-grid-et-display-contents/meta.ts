9c562b2574db2c2dd0906097820eec9d874a45e6
Julien Pradet <julien.pradet@gmail.com>
2023-08-28 11:20:32 +0200

    Fix meta description

--- src/routes/(blog)/fiches-techniques/aligner-avec-css-grid-et-display-contents/meta.ts
+++ src/routes/(blog)/fiches-techniques/aligner-avec-css-grid-et-display-contents/meta.ts
@@ -6,7 +6,7 @@ const meta: ArticleMeta = {
 	isListed: true,
 	title: 'Aligner des éléments avec CSS Grid et display: contents',
 	date: new Date('2023-08-28'),
-	description: `Dans vos maquettes, les éléments sont parfaitement alignés. Mais une fois les vrais contenus présents, rien ne va. Voyons comment, en attendant l'arrivée des subgrid, comment régler ce problème.`,
+	description: `Dans vos maquettes, les éléments sont parfaitement alignés. Mais une fois les vrais contenus présents, rien ne va. Voyons, en attendant l'arrivée des subgrid, comment régler ce problème.`,
 	category: ArticleCategory['fiches-techniques']
 };
 
