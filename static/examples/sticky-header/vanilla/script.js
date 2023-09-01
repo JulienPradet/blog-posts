e83f55a7936e279a073ae6af017619843d9b4031
Julien Pradet <julien.pradet@gmail.com>
2023-09-01 21:48:19 +0200

    Better handle device resize which is a lot of complexity on its own ^^'

--- static/examples/sticky-header/vanilla/script.js
+++ static/examples/sticky-header/vanilla/script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	const mainHeader = document.querySelector('.main-header');
 	const banner = document.querySelector('.main-header > img');
 
-	mainHeader.style.minHeight = `${mainHeader.clientHeight}px`;
+	let isAnimating = false;
 
 	const updateStickyAuthorIfNeeded = function (isSticky) {
 		if (
@@ -16,8 +16,13 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 
 		const elementsToAnimate = author.querySelectorAll('.js-animate');
+
+		isAnimating = true;
 		animate(elementsToAnimate, () => {
 			author.classList.toggle('author--sticky', isSticky);
+		}).then(() => {
+			isAnimating = false;
+			updateMinHeightIfNeeded();
 		});
 	};
 
@@ -29,9 +34,64 @@ document.addEventListener('DOMContentLoaded', () => {
 		},
 		{
 			threshold: 0,
-			rootMargin: '-30px 0px'
+			// The reason we choose 32 here is because :
+			// - the avatar in its non sticky version is 8rem (= 128px) height
+			// - only half of it overlaps on the cover image (64px)
+			// - and we don't want the sticky to be displayed too early to avoid so we only take half of it
+			// In practice it means that the animation triggers once 1/4 of the height of the avatar has
+			// been hidden
+			rootMargin: '-32px 0px'
 		}
 	);
 
 	observer.observe(banner);
+
+	// If you only care about the animation you can ignore the following part.
+	// However, a final thing we will need to take care of is the resize of a device
+	// For instance when a mobile device goes from portrait mode to landscape mode,
+	// the minHeight will likely chance since the title "Enchanté, Julien Pradet"
+	// will go from 2 lines to 1 line.
+	// However we don't want to be too eager, there are a bunch of reason a resize
+	// can happen be we don't want to recompute the minHeight:
+	// - if the author just passed in sticky mode, it'll trigger a resize even though the minHeight shouldn't change
+	// - if an animation was just triggered, it's too early to compute the minHeight. we'll have to wait for the animation to finish - this is why we've added a then to the `animate` above
+	// - the view port only changed in height, not in width
+	let previousBannerHeight = 0;
+	let previousAuthorHeight = 0;
+	function updateMinHeightIfNeeded() {
+		const isSticky = author.classList.contains('author--sticky');
+		if (isSticky) {
+			// Not useful to recalculate minHeight because the author
+			// is displayed in a minimized style
+			return;
+		}
+
+		// There is no need to update the total minHeight if neither the author nor the cover image
+		// changed its height.
+		let shouldUpdateMainHeaderMinHeight = false;
+		if (author.getBoundingClientRect().height !== previousAuthorHeight) {
+			shouldUpdateMainHeaderMinHeight = true;
+		} else if (banner.getBoundingClientRect().height !== previousBannerHeight) {
+			shouldUpdateMainHeaderMinHeight = true;
+		}
+
+		if (!shouldUpdateMainHeaderMinHeight) {
+			return;
+		}
+
+		previousAuthorHeight = author.getBoundingClientRect().height;
+		previousBannerHeight = banner.getBoundingClientRect().height;
+		mainHeader.style.minHeight = null;
+		mainHeader.style.minHeight = `${mainHeader.clientHeight}px`;
+	}
+
+	const resizeObserver = new ResizeObserver(async () => {
+		if (isAnimating) {
+			return;
+		}
+
+		updateMinHeightIfNeeded();
+	});
+
+	resizeObserver.observe(document.body);
 });
