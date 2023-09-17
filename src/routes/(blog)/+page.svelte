f5ea8e9accc9de4ee6f0f6ecf530a1734347de65
Julien Pradet <julien.pradet@gmail.com>
2023-09-17 08:56:24 +0200

    Add popular articles at the top of the homepage

--- src/routes/(blog)/+page.svelte
+++ src/routes/(blog)/+page.svelte
@@ -55,7 +55,78 @@
 </Content>
 
 <MainContent>
+	<div class="summary">
+		<Content>
+			<h2 class="content-title">
+				Pour découvrir la <abbr title="Web Performance">WebPerf</abbr>
+			</h2>
+
+			<ul>
+				<li>
+					<a href="/tutoriels/pourquoi-ameliorer-le-lcp/">
+						Pourquoi et comment accélérer le chargement de vos pages ?
+					</a>
+					(juil. 2023)
+					<p>
+						Comprendre pourquoi la WebPerf c'est important et découvrir les outils pour démarrer
+					</p>
+				</li>
+				<li>
+					<a href="/tutoriels/reseau-et-core-web-vitals/">
+						Analyser et accélérer le chargement d'une page
+					</a>
+					(juil. 2023)
+					<p>
+						Apprendre à lire les audits de performance afin d'être capable de comprendre quoi
+						améliorer sur votre site
+					</p>
+				</li>
+				<li>
+					<a href="/tutoriels/optimiser-le-chargement-des-images/">
+						Optimiser les images sur le web : checklist des bonnes pratiques
+					</a>
+					(juil. 2023)
+					<p>
+						Bien comprendre les techniques d'optimisation afin de ne plus servir d'images
+						sous-optimisées
+					</p>
+				</li>
+			</ul>
+		</Content>
+
+		<Content>
+			<h2 class="content-title">Mes articles préférés</h2>
+
+			<ul>
+				<li>
+					<a href="/tutoriels/mener-au-changement/"> Mener au changement </a>
+					(juil. 2023)
+					<p>
+						Un retour d'expérience sur comment apporter du changement au sein d'une équipe étape par
+						étape
+					</p>
+				</li>
+				<li>
+					<a href="/tutoriels/sticky-header-avec-hauteur-dynamique-performant/">
+						Comment faire un header sticky animé et performant ?
+					</a>
+					(sept. 2023)
+					<p>Un condensé d'astuces front-end pour faire des animations front au quotidien</p>
+				</li>
+				<li>
+					<a href="/tutoriels/des-animations-performantes-1/"> Des animations performantes </a>
+					(fev. 2017)
+					<p>Plonger dans le fonctionnement des navigateurs pour en tirer des animations à 60fps</p>
+				</li>
+			</ul>
+		</Content>
+	</div>
+
+	<hr />
+
 	<Content>
+		<h2 class="content-title">Tous les articles</h2>
+
 		<ArticleList {articles} />
 	</Content>
 
@@ -63,3 +134,35 @@
 
 	<FollowIncentive />
 </MainContent>
+
+<style>
+	h2.content-title {
+		margin-bottom: 2rem;
+	}
+
+	.summary {
+		display: flex;
+		flex-direction: column;
+		gap: 2rem 4rem;
+	}
+
+	.summary ul {
+		padding: 0;
+		margin-bottom: 0;
+	}
+
+	.summary li {
+		margin: 0.5rem 0;
+		list-style: none;
+	}
+
+	.summary li p {
+		margin: 0;
+	}
+
+	@media (min-width: 75rem) {
+		.summary {
+			flex-direction: row;
+		}
+	}
+</style>
