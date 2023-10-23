1a5ed55b2b528d2f1af476d329246ab5d31baf53
Julien Pradet <julien.pradet@gmail.com>
2023-10-23 10:23:06 +0200

    Fix typo in article description

--- src/routes/(blog)/tutoriels/comment-alleger-son-javascript/meta.ts
+++ src/routes/(blog)/tutoriels/comment-alleger-son-javascript/meta.ts
@@ -6,7 +6,7 @@ const meta: ArticleMeta = {
 	isListed: true,
 	title: 'Comment alléger son JavaScript',
 	date: new Date('2023-10-23'),
-	description: `Quand on parle web performance, dans le web d'aujourd'hui, le plus grand coupable est souvent JavaScript. Voyons ensemble quelles techniques pour optimiser celui-ci.`,
+	description: `Quand on parle web performance, dans le web d'aujourd'hui, le plus grand coupable est souvent JavaScript. Voyons ensemble les techniques pour optimiser celui-ci.`,
 	category: ArticleCategory['tutoriels']
 };
 
